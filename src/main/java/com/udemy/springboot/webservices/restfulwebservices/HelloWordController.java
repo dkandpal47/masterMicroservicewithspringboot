1ba3576b4a9959879f4325835a5aa9b19c9a57d1
dinesh kandpal <dinesh.kandpal47@gmail.com>
2020-06-05 11:25:38 +0530

    first commit

---
+++ src/main/java/com/udemy/springboot/webservices/restfulwebservices/HelloWordController.java
@@ -0,0 +1,32 @@
+package com.udemy.springboot.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+	//Define GET 
+	//Define Method 
+	//Map URI with method
+	
+	@RequestMapping(method=RequestMethod.GET,path="/hello-word")
+	public String helloWord() {
+		return "=======> Hello World Dinesh Kandpal \n without bean <======";
+	}
+	
+	@RequestMapping(method=RequestMethod.GET,path="/hello-word-bean")
+	public HelloWorldBean helloWordBean() {
+		return new HelloWorldBean("Hello World (Bean)");
+	}
+	
+	//with pth varible /hello-word/path-variable/dinesh
+	//Without using @PathVariable annotion with bean it will pass null
+	@GetMapping(path="/hello-word/{name}")
+	public HelloWorldBean helloWordBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello %s in the World",name));
+	}
+}
---
+++ src/main/java/com/udemy/springboot/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.udemy.springboot.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+		
+	private String message;
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	//Without getter won't work , end point will throw exception
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+}
